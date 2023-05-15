b2340472f845a177e9c0df26a9ce25b4ea2534e3
4332001876 <mzfslhm@163.com>
2023-05-15 21:55:09 +0800

    lab3

--- lab3/server/src/main.rs
+++ lab3/server/src/main.rs
@@ -12,7 +12,7 @@ struct RequestInfo {
     host: String,
 }
 impl RequestInfo {
-    fn new( ) -> Self {
+    fn new() -> Self {
         RequestInfo {
             method: String::from(""),
             uri: String::from(""),
@@ -57,33 +57,52 @@ fn handle_clnt(mut stream: TcpStream) {
     ];
     let request_info = parse_request(&http_request1);
     println!("{:?}",request_info);*/
-    let mut status_code=200;
+    let mut status_code = 200;
+    /*200 OK 请求已成功，请求所希望的响应头或数据体将随此响应返回。
+    404 Not Found 请求失败，请求所希望得到的资源未被在服务器上发现。
+    500 Internal Server Error 本实验中未定义的各种错误。
+    */
 
-    
-    let mut request_info=RequestInfo::new();
-    match parse_request(&http_request){
-        Some(rq_info) => request_info=rq_info,
-        None =>  status_code=500,//500 Internal Server Error
+    let mut request_info = RequestInfo::new();
+    match parse_request(&http_request) {
+        Some(rq_info) => request_info = rq_info,
+        None => status_code = 500, //500 Internal Server Error
     }
     println!("{:?}", request_info);
-
-    let content_read = get_uri_content(&request_info.uri, "/webroot");
-    let mut content_len=0;
-    
-    /*match content_read{
-        Ok(contents)=>{content_len=contents.len();
-        Err=>
+    let mut status_line = String::from("HTTP/1.0 ");
+    let mut content_len = 0;
+    let mut contents: Vec<u8> = Vec::new();
+    if (status_code == 200) {
+        let content_read = get_uri_content(&request_info.uri, "./webroot");
+        //println!("{:?}", content_read);
+        contents = match content_read {
+            Ok(content) => {
+                content_len = contents.len();
+                content
+            }
+            Err(_) => {
+                status_code = 404;
+                Vec::new()
+            }
+        };
     }
-
-    let length = contents.len();
-
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");*/
-
+    if (status_code == 200) {
+        status_line.push_str("200 OK");
+    } else if (status_code == 404) {
+        status_line.push_str("404 Not Found");
+    } else {
+        status_line.push_str("500 Internal Server Error");
+    }
+    let response = format!("{status_line}\r\nContent-Length: {content_len}\r\n\r\n");
+    stream
+        .write_all(&[response.as_bytes(), &contents].concat())
+        .unwrap();
+    //println!("{:?}", &[response.as_bytes(), &contents].concat());
+    //stream.write_all(&contents).unwrap();
     //bytes: &[u8]
     //vec.append(&mut vec2);
 }
-fn get_uri_content(uri: &str, web_root: &str) -> Result<Vec<u8>,std::io::Error> {
+fn get_uri_content(uri: &str, web_root: &str) -> Result<Vec<u8>, std::io::Error> {
     let mut final_uri = String::from(web_root);
     final_uri.push_str(uri);
     let idx = (final_uri.len() - 1);
@@ -92,15 +111,16 @@ fn get_uri_content(uri: &str, web_root: &str) -> Result<Vec<u8>,std::io::Error> 
     if &final_uri[idx..] == "/" {
         final_uri.push_str("index.html");
     }
-    fs::read("hello.html")
+    //println!("{:?}", final_uri);
+    fs::read(final_uri)
 }
 fn parse_request(request_lines: &Vec<String>) -> Option<RequestInfo> {
     if request_lines.len() < 2 {
-        return None
+        return None;
     }
     let request: Vec<_> = request_lines[0].split(" ").collect(); //请求头
     if request.len() != 3 {
-        return None
+        return None;
     }
 
     let method_name;
@@ -112,7 +132,7 @@ fn parse_request(request_lines: &Vec<String>) -> Option<RequestInfo> {
     let host_info: Vec<_> = request_lines[1].split("Host:").collect(); //请求头
                                                                        //println!("{:?}", host_info);
     if host_info.len() != 2 {
-        return None
+        return None;
     } //["", " 127.0.0.1:8000"]
 
     Some(RequestInfo {
